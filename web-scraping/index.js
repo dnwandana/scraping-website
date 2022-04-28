1b73c209aed61a7d7cfb4e94239e939efa1f0a67
zaqijr7 <zaqijr7@gmail.com>
2022-04-28 10:25:09 +0700

    fixing conflict

--- web-scraping/index.js
+++ web-scraping/index.js
@@ -4,17 +4,20 @@ const ObjectsToCsv = require('objects-to-csv');
 async function indexing(url){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, {
+    waitUntil: 'load',
+    timeout: 0,
+  });
 
   const link = await linking(page);
   const data = [];
   let newListLink = [];
-  const scrapingData1 = await scraping(link);
+  const scrapingData1 = await scraping(link, browser);
   data.push(...scrapingData1.data);
   newListLink.push(...scrapingData1.newListLink);
   let newListUniqueLink = await filterUniqLink(newListLink)
   newListLink.push(...newListUniqueLink)
-  const scrapingData2 = await scraping(newListUniqueLink);
+  const scrapingData2 = await scraping(newListUniqueLink, browser);
   data.push(...scrapingData2.data);
   newListLink.push(...scrapingData2.newListLink);
 
@@ -50,42 +53,49 @@ async function filterUniqLink(data) {
   })
 }
 
-async function scraping(link){
+async function scraping(link, browser){
   const newListLink = [];
   const data = [];
   await Promise.all(link.map(async (e, index) => {
-    if(!e.split('/').includes('unanswered')){
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(e);
-
-      const newLink = await page.evaluate(() => {
-        return Array.from(document.getElementsByClassName('q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--none Link___StyledBox-t2xg9c-0')).map((x) => {
-          return x.href
+    try {
+      if(!e.split('/').includes('unanswered')){
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto(e, {
+          waitUntil: 'load',
+          timeout: 0,
+        });
+  
+        const newLink = await page.evaluate(() => {
+          return Array.from(document.getElementsByClassName('q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--none Link___StyledBox-t2xg9c-0')).map((x) => {
+            return x.href
+          })
         })
-      })
-
-      newListLink.push(...newLink);
-
-      const answer = await page.evaluate(() => {
-        return Array.from(document.getElementsByClassName('q-box qu-userSelect--text')).map((x) => {
-          return x.textContent
+  
+        newListLink.push(...newLink);
+  
+        const answer = await page.evaluate(() => {
+          return Array.from(document.getElementsByClassName('q-box qu-userSelect--text')).map((x) => {
+            return x.textContent
+          })
         })
-      })
-      try {
-        if (answer[1].includes('Lanjutkan Membaca')) {
-          console.log(answer[1].includes('Lanjutkan Membaca'));
-          data.push({question: answer[0], answer: answer[2]})
-        } else {
-          // console.log(false);
-          data.push({question: answer[0], answer: answer[1]})
+        try {
+          if (answer[1].includes('Lanjutkan Membaca')) {
+            console.log(answer[1].includes('Lanjutkan Membaca'));
+            data.push({question: answer[0], answer: answer[2]})
+          } else {
+            // console.log(false);
+            data.push({question: answer[0], answer: answer[1]})
+          }
+        } catch {
+          console.log("Something Error, please try again");
         }
-      } catch {
-        console.log("Something Error, please try again");
-        browser.close();
       }
-      browser.close();
+    } catch (error) {
+      console.log(error);
+      console.log("puppeter error");
     }
+    
   }))
 
   return {
