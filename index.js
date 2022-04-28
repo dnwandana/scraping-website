a27b475a3f04c7b13582eee46ac426e4923ebeee
Wandana <dn.wandana@gmail.com>
2022-04-28 15:58:01 +0700

    add prettier and read url from args

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs/promises')
-const ObjectsToCsv = require('objects-to-csv');
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
+const ObjectsToCsv = require("objects-to-csv");
 require("dotenv/config");
 const QandAnswerController = require("./controllers/qAndAnserController");
 
-async function indexing(url){
+async function indexing(url) {
   const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
   await page.goto(url, {
-    waitUntil: 'load',
+    waitUntil: "load",
     timeout: 0,
   });
 
@@ -19,96 +19,108 @@ async function indexing(url){
   const scrapingData1 = await scraping(link, browser);
   data.push(...scrapingData1.data);
   newListLink.push(...scrapingData1.newListLink);
-  let newListUniqueLink = await filterUniqLink(newListLink)
-  newListLink.push(...newListUniqueLink)
+  let newListUniqueLink = await filterUniqLink(newListLink);
+  newListLink.push(...newListUniqueLink);
   const scrapingData2 = await scraping(newListUniqueLink, browser);
   data.push(...scrapingData2.data);
   newListLink.push(...scrapingData2.newListLink);
 
   // convertToCSV(data)
-  await QandAnswerController.insertMany(data)
+  await QandAnswerController.insertMany(data);
   console.log("finished");
 
   await browser.close();
+
+  // end process
+  process.exit(1);
 }
 
 const convertToCSV = async (data) => {
   const csv = new ObjectsToCsv(data);
- 
+
   // // Save to file:
   await csv.toDisk(`./results/${Date.now()}test.csv`);
-}
-
+};
 
 async function linking(page) {
   const link = await page.evaluate(() => {
-    return Array.from(document.getElementsByClassName('q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--none Link___StyledBox-t2xg9c-0')).map((x) => {
-      return x.href
-    })
-  })
-  const uniqueLink = link.filter(function(item, pos) {
+    return Array.from(
+      document.getElementsByClassName(
+        "q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--none Link___StyledBox-t2xg9c-0"
+      )
+    ).map((x) => {
+      return x.href;
+    });
+  });
+  const uniqueLink = link.filter(function (item, pos) {
     return link.indexOf(item) == pos;
-  })
+  });
 
   return uniqueLink;
 }
 
 async function filterUniqLink(data) {
-  return data.filter(function(item, pos) {
+  return data.filter(function (item, pos) {
     return data.indexOf(item) == pos;
-  })
+  });
 }
 
-async function scraping(link, browser){
+async function scraping(link, browser) {
   const newListLink = [];
   const data = [];
-  await Promise.all(link.map(async (e, index) => {
-    try {
-      if(!e.split('/').includes('unanswered')){
-        const page = await browser.newPage();
-        await page.setDefaultNavigationTimeout(0);
-        await page.goto(e, {
-          waitUntil: 'load',
-          timeout: 0,
-        });
-  
-        const newLink = await page.evaluate(() => {
-          return Array.from(document.getElementsByClassName('q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--none Link___StyledBox-t2xg9c-0')).map((x) => {
-            return x.href
-          })
-        })
-  
-        newListLink.push(...newLink);
-  
-        const answer = await page.evaluate(() => {
-          return Array.from(document.getElementsByClassName('q-box qu-userSelect--text')).map((x) => {
-            return x.textContent
-          })
-        })
-        try {
-          if (answer[1].includes('Lanjutkan Membaca')) {
-            console.log(true);
-            data.push({question: answer[0], answer: answer[2]})
-          } else {
-            console.log(false);
-            data.push({question: answer[0], answer: answer[1]})
+  await Promise.all(
+    link.map(async (e, index) => {
+      try {
+        if (!e.split("/").includes("unanswered")) {
+          const page = await browser.newPage();
+          await page.setDefaultNavigationTimeout(0);
+          await page.goto(e, {
+            waitUntil: "load",
+            timeout: 0,
+          });
+
+          const newLink = await page.evaluate(() => {
+            return Array.from(
+              document.getElementsByClassName(
+                "q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--none Link___StyledBox-t2xg9c-0"
+              )
+            ).map((x) => {
+              return x.href;
+            });
+          });
+
+          newListLink.push(...newLink);
+
+          const answer = await page.evaluate(() => {
+            return Array.from(
+              document.getElementsByClassName("q-box qu-userSelect--text")
+            ).map((x) => {
+              return x.textContent;
+            });
+          });
+          try {
+            if (answer[1].includes("Lanjutkan Membaca")) {
+              console.log(true);
+              data.push({ question: answer[0], answer: answer[2] });
+            } else {
+              console.log(false);
+              data.push({ question: answer[0], answer: answer[1] });
+            }
+          } catch {
+            console.log("Something Error, please try again");
           }
-        } catch {
-          console.log("Something Error, please try again");
         }
+      } catch (error) {
+        console.log("puppeter error");
       }
-    } catch (error) {
-      console.log("puppeter error");
-    }
-    
-  }))
+    })
+  );
 
   return {
     data,
-    newListLink
-  }
+    newListLink,
+  };
 }
-
 
 // async function main(){
 //   const file = await fs.readFile('./linking.txt', {encoding: 'utf-8'});
@@ -117,8 +129,9 @@ async function scraping(link, browser){
 //     if (link.length) {
 //       await indexing(link, browser)
 //     }
-//   })) 
+//   }))
 // }
 
-indexing('https://id.quora.com/Apa-perasaamu-sebagai-lulusan-S-2-tapi-menganggur')
+const args = process.argv.slice(2);
+indexing(args[0]);
 // main()
--- models/qAndAnswer.js
+++ models/qAndAnswer.js
@@ -2,7 +2,7 @@ const mongoose = require("../config/mongodb");
 
 const questionsSchema = new mongoose.Schema(
   {
-    question: { type: String, index: true, },
+    question: { type: String, index: true },
     answer: { type: String },
   },
   { timestamps: true, collection: "questions" }
